db3c47486951155115939fc36bef2ea1aa27f59c
doron segal <segal.doron@gmail.com>
2018-04-10 12:22:45 +0300

    update

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            // }, {
+            //     test: /\.(ttf|eot|woff|woff2)$/,
+            //     loader: "file-loader",
+            //     options: {
+            //         name: "fonts/[name].[ext]",
+            //     },
+            },
+        ]
+    },
+    devtool: 'source-map',
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })],
+    devServer: {
+        historyApiFallback: true
+    }
+}
